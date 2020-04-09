78081600ce2ead76ce656d85d4d3be905d6b267a
Bastien <bastien@MacBook-Pro-de-Basty.local>
2020-04-08 21:19:30 -0400

    Updates

---
+++ precache-manifest.b0f20e16097c63d603e1a5fde471afc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7c40c52bf688987585c3fd3eec345b",
+    "url": "/mrbasty/movieapp/index.html"
+  },
+  {
+    "revision": "8c6093554729eb69b589",
+    "url": "/mrbasty/movieapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af13450831f61a676933",
+    "url": "/mrbasty/movieapp/static/js/2.50797b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrbasty/movieapp/static/js/2.50797b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6093554729eb69b589",
+    "url": "/mrbasty/movieapp/static/js/main.a7c09180.chunk.js"
+  },
+  {
+    "revision": "efa45bdaadb889e62e9a",
+    "url": "/mrbasty/movieapp/static/js/runtime-main.74cf71e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mrbasty/movieapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
